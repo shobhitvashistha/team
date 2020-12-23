aaeb5a2a63f7d266e06aed21b97369d75f3161d2
Shobhit Vashistha <shobhit.v87@gmail.com>
2020-12-23 16:21:04 +0530

    wider support for different formats of phone numbers added by using django-phonenumbers-field library

--- members/models.py
+++ members/models.py
@@ -2,20 +2,17 @@ from django.core.validators import RegexValidator
 from django.db import models
 
 # Create your models here.
+from phonenumber_field.modelfields import PhoneNumberField
 
 ROLE_CHOICES = (
     ('admin', 'admin'),
     ('regular', 'regular')
 )
 
-PHONE_VALIDATOR = RegexValidator(
-    regex=r'^\+?\d{8,15}$',
-    message="Phone number must be entered in the format: '+999999999'. 8-15 digits allowed.")
-
 
 class TeamMember(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=16, unique=True, validators=[PHONE_VALIDATOR])
+    phone_number = PhoneNumberField(unique=True)
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
